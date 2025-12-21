9086087a588d1a6b9aa6c055f9694255a7d92d9c
tashidzen <tashidzen@yandex.ru>
2025-12-22 02:30:54 +0300

    Реализован переход на страницу с постами отдельного пользователя.

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -3,14 +3,14 @@ import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage } from "../index.js";
 
 export function renderPostsPageComponent({ appEl }) {
-  // @TODO: реализовать рендер постов из api
+  // реализован рендер постов из api
 
   console.log("Актуальный список постов:", posts);
 
   const postsHtml = posts.map((post) => {
    return `
                   <li class="post">
-                    <div class="post-header" data-user-id=:"${post.id}">
+                    <div class="post-header" data-user-id=${post.user.id}>
                         <img src="${post.user.imageUrl}" class="post-header__user-image">
                         <p class="post-header__user-name">${post.user.name}</p>
                     </div>
@@ -18,7 +18,7 @@ export function renderPostsPageComponent({ appEl }) {
                       <img class="post-image" src="${post.imageUrl}">
                     </div>
                     <div class="post-likes">
-                      <button data-post-id="${post.likes.id}" class="like-button">
+                      <button data-post-id=${post.likes.id} class="like-button">
                         <img src="./assets/images/like-active.svg">
                       </button>
                       <p class="post-likes-text">
@@ -34,7 +34,7 @@ export function renderPostsPageComponent({ appEl }) {
                     </p>
                   </li>
     `;
-  });
+  }).join("");
   /**
    * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
